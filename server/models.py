5edc32ecceeef2e7c94457b11911ce160b746aba
Dylan Tin <dylant711@gmail.com>
2024-10-30 15:05:50 -0400

    relationships lab

--- server/models.py
+++ server/models.py
@@ -12,21 +12,51 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # define a model class by inheriting from db.Model.
-class Flight(db.Model):
+class Flight(db.Model, SerializerMixin):
     __tablename__ = 'flights'
+
+    serialize_rules = ("-bookings.flight", "-bookings.customer")
 
     id = db.Column(db.Integer, primary_key=True)
     airline = db.Column(db.String)
 
     def __repr__(self):
         return f'<Flight {self.id}, {self.airline}>'
+    
+    bookings = db.relationship('Booking', back_populates='flight')
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+
+    serialize_rules = ("-bookings.flight", "-bookings.customer")
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
 
+    bookings = db.relationship('Booking', back_populates='customer')
+
+    # Task 2 - Association Proxy
+    customers = association_proxy('bookings', 'customer', creator=lambda c: Booking(customer=c))
+
+    hotels = association_proxy('bookings', 'flight', creator=lambda h: Booking(hotels=h))
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.first_name} {self.last_name}>'
+    
+
+    
+class Booking(db.Model, SerializerMixin):
+    __tablename__ = 'bookings'
+
+    serialize_rules = ("-customer.bookings", "-flight.bookings")
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float)
+    destination = db.Column(db.String)
+    flight_id = db.Column(db.Integer, db.ForeignKey('flights.id'))
+    customer_id = db.Column(db.ForeignKey('customers.id'))
+
+    flight = db.relationship('Flight', back_populates='bookings')
+    customer = db.relationship('Customer', back_populates='bookings')
+
